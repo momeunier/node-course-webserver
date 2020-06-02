7d5210175309291b168545fc03421c54f7c46c19
Marc-Olivier Meunier <marc@smartly.io>
2020-06-02 19:58:38 +0300

    heroku config

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,13 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     messageOne.textContent = 'Loading'
     messageTwo.textContent = ''
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
